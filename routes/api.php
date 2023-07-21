f5d783d66f150f62ec226239e185d3be6af18839
Nathan <nathanrtd01@gmail.com>
2023-07-21 16:30:16 -0300

    Feat: add Routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +15,41 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware('auth:api')->get('/test', function() {
+    return "HELLOW";
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::prefix('/category')->group(function () {
+    Route::get('', [CategoryController::class, 'index']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::post('/store', [CategoryController::class, 'store']);
+    Route::put('/update/{id}', [CategoryController::class, 'update']);
+    Route::delete('/destroy/{id}', [CategoryController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'product', 'middleware' => ['auth:api']], function () {
+    Route::get('', [ProductController::class, 'index']);
+    Route::get('/{id}', [ProductController::class, 'show']);
+    Route::post('/store', [ProductController::class, 'store']);
+    Route::put('/update/{id}', [ProductController::class, 'update']);
+    Route::delete('/destroy/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::group(['prefix' => 'user', 'middleware' => ['auth:api']], function () {
+    Route::get('', [UserController::class, 'index']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::post('/store', [UserController::class, 'store']);
+    Route::put('/update/{id}', [UserController::class, 'update']);
+    Route::delete('/destroy/{id}', [UserController::class, 'destroy']);
+});
+
+Route::group(['prefix' => '/auth'], function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
 });
